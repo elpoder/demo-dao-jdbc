a9c61200c260e9107e030b23f2e58b2dc4dc04ea
Obiwan <obiwan@localhost.localdomain>
2021-05-11 21:57:16 -0300

    commit classe seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
 
 public class Program {
@@ -7,7 +9,9 @@ public class Program {
 	public static void main(String[] args) {
 		
 		Department obj = new Department(1, "books");
-		System.out.println(obj);
+		
+		Seller seller = new Seller(1, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		System.out.println(seller);
 		
 	}
 
